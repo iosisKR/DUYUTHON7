9be16f8bdbc20400597cb2bfdd9faf161bf393fe
iosis <91136115+iosisKR@users.noreply.github.com>
2026-05-25 10:53:24 +0900

    Update aiController.js

--- controllers/aiController.js
+++ controllers/aiController.js
@@ -1,6 +1,7 @@
 import { askGeminiForAlarm, askGeminiForStock, askGeminiForMove } from "../services/gemini.js";
 
 export async function analyzeTrash(req, res) {
+  console.log("가라 환경 정화 분석!")
   const test = req.body.text.split("!AND!");
   const text = test[0];
   const purpose = test[1];
@@ -12,6 +13,7 @@ export async function analyzeTrash(req, res) {
 }
 
 export async function analyzeStock(req, res) {
+  console.log("가라 재고 분석!")
   const result = await askGeminiForStock(rentalHistory);
   console.log(result);
   res.json({
@@ -20,10 +22,10 @@ export async function analyzeStock(req, res) {
 }
 
 export async function analyzeMove(req, res) {
+  console.log("가라 이동 분석!")
   const result = await askGeminiForMove(rentalHistory);
   console.log(result);
   res.json({
     result,
   });
 }
-

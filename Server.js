97c7843c0555c2e4dbceae79d1e026bb7d65841b
iosis <91136115+iosisKR@users.noreply.github.com>
2026-05-25 14:03:32 +0900

    Update Server.js

--- Server.js
+++ Server.js
@@ -64,9 +64,10 @@ export const rentalHistory = [ //임시 데이터
 
 // ===== 미들웨어 (순서 중요!) =====
 app.use(cors({ credentials: true, origin: "https://duyuthon7.onrender.com:3000" }));
-app.use(cors({ credentials: true, origin: "https://localhost:5173" }));
-app.use(cors({ credentials: true, origin: "https://localhost:3000" }));
+//app.use(cors({ credentials: true, origin: "https://localhost:5173" }));
+//app.use(cors({ credentials: true, origin: "https://localhost:3000" }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ===== API 라우터 등록 =====
 app.use("/ai", aiRoutes);                       // AI 관련 (기존)
